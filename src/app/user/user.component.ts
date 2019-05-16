cd3bc8a4b922028988e3f00b5f0b977f863d92e0
Vladimir Kosenko <vladimir.kosnko8@gmail.com>
2019-05-16 09:25:58 +0300

    User.component do not work

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,17 +1,47 @@
-import { Component, OnInit } from '@angular/core';
-import { MatTabChangeEvent } from '@angular/material';
+import {MediaMatcher} from '@angular/cdk/layout';
+import {ChangeDetectorRef, Component, OnDestroy} from '@angular/core';
 
+/** @title Responsive sidenav */
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
 
-  public tabChanged(tabChangeEvent: MatTabChangeEvent): void {
-    console.log(tabChangeEvent);
+export class UserComponent implements OnDestroy {
+  mobileQuery: MediaQueryList;
+
+  private _mobileQueryListener: () => void;
+
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
   }
-  constructor() {}
 
-  ngOnInit() {}
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
+
+  shouldRun: boolean = true;
 }
+
+/////////////////////////////
+// import { Component, OnInit } from '@angular/core';
+// import { MatTabChangeEvent } from '@angular/material';
+
+// @Component({
+//   selector: 'app-user',
+//   templateUrl: './user.component.html',
+//   styleUrls: ['./user.component.css']
+// })
+// export class UserComponent implements OnInit {
+
+//   public tabChanged(tabChangeEvent: MatTabChangeEvent): void {
+//     console.log(tabChangeEvent);
+//   }
+//   constructor() {}
+
+//   ngOnInit() {}
+// }
+
